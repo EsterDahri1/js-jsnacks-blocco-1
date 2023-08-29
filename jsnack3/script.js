4fe941a90962c1581b6dd164ee73816d481d001a
EsterDahri <esterdahri@gmail.com>
2023-08-29 16:16:04 +0200

    add times to ask q and for loop

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,21 @@
 console.log('testing hello');
 
-/* #snack 3*/
+/* #snack 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+//enunciate how many times would you like to ask the user to insert a number
+const howManyTimes = 10;
+console.log(howManyTimes);
+
+//create a loop with for
+for(let i = 0; i < howManyTimes; i++){
+    const userAnswers = Number(prompt('Insert a number'));
+    console.log(userAnswers);
+}
+//add a prompt with the question 
+
+//sum the inserted numbers 
+
+//print on page the result

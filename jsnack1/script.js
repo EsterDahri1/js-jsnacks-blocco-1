0e3f93b7bd4c4022c4d98cbc2271d4d406aaa7e7
EsterDahri <esterdahri@gmail.com>
2023-08-29 15:24:58 +0200

    add the two numbers

---
+++ jsnack1/script.js
@@ -0,0 +1,23 @@
+console.log('hello!');
+
+/* #Snack 1
+
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+/* Tools:
+- prompt 1
+- prompt 2
+- log
+- if
+*/
+
+
+//ask user the first number
+const firstNumber = Number(prompt('Insert a number of your choice'));
+
+//ask user the second number
+const secondNumber = Number(prompt('Insert another number of your choice'));
+
+console.log(firstNumber, secondNumber);
